a55381e5a0d8935119b8a50075461c906d0e1a7c
sakshijaiswal9889 <sakshijaiswal9889@gmail.com>
2024-01-09 11:03:55 +0530

    first

---
+++ introduction.js
@@ -0,0 +1,129 @@
+// // var a = "ritesh";
+// // var password = "cap&&$%321";
+// // var sentence = "ritesh is a good boy";
+// // var number = 12;
+
+// // // print jo bhi value hai a mein
+
+
+// // console.log(a)
+// // console.log(number)
+
+
+// // variable syntax variable name = varable's value;
+
+
+// //how to create variables . 
+
+// // var a = 19;
+
+// //Declaration phase ; 
+// // var a ;
+// // //Initialization phase;
+// // a = 10;
+// // //Assignment phase (reassignment phase);
+// // a = 11;
+
+// // console.log(a)
+
+// // l =>  right
+
+// // t => bottom
+
+  
+// //how to print variables .
+// //what are datatypes.
+
+
+// // signup system.
+
+// var name ;
+
+// name = "ritesh"
+
+// name = "shripad"
+
+// name = "chota bheem"
+
+// name = "X0X"
+
+// name = 1234
+
+// name = "hello"
+
+// // in js we can reassign the value of a variable irrespective of it's previous datatypes.
+
+// // HW => Loosely typed languages and strictly typed languages.
+
+// console.log(name)
+
+// //datatype => number , string
+
+// // var a = 12;
+// // var b = "name"
+
+
+
+
+// var xyz = 131 ;  //number
+
+// xyz = 121 ; 
+
+// xyz = "ritesh@@@@####123489432"
+
+// console.log(xyz)
+
+
+// var a = 1;
+// var b = 2;
+// var c = 3;
+// var d = 4;
+
+// console.log(a , b , c ,d)
+
+// console.log(a+b)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+
+// case sensitive
+
+
+// var a = 1;
+// var A = 2;
+// console.log(A)
+
+// var a = 1;
+// console.log(_name)
+
+// $ _
+
+// var 1 = 1;  not at all , not following naming convention
+
+
+// var a = 20;
+
+// // var b = 30 ; 
+
+// // b = 40;
+
+// a = "30"
+
+// a = 12345
+// console.log(typeof a , a )
+
+// var typeof = 20 ; 
+
+
+// typeof is part of something that is called inbuilt functions
+
+var  a = 10;
+let b = 20;
+// const c = 30
+
+console.log(a,b)
+
+
+
+
+  
